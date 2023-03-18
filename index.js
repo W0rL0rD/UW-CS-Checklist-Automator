c6d47ec4acb9d1f15483db5a186e9b9b01a3364c
Pranav <1pranavvyas@gmail.com>
2023-03-18 03:46:13 -0400

    Returns website on get req

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var PORT = process.env.PORT || 8080;
 
 const server = http.Server(app).listen(PORT);
 
+app.use(express.static(__dirname + '/webpage'));
+
 app.get("/", (req, res)=>{
+    console.log("Hello world");
     res.sendFile(__dirname + "/webpage/signup.html");
 });
