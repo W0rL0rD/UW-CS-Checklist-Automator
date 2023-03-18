032f73636bc963bee34de786bf8b670cb0fb842e
Pranav <1pranavvyas@gmail.com>
2023-03-18 05:17:18 -0400

    Received input from login forms

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 var PORT = process.env.PORT || 8080;
 
 const server = http.Server(app).listen(PORT);
 
-app.use(express.static(__dirname + '/webpage'));
+var urlencodedparser = bodyParser.urlencoded({extended:false});
+
+app.use(express.static('Images'));
+app.use(express.static('webpage'));
 
 app.get("/", (req, res)=>{
     console.log("Hello world");
     res.sendFile(__dirname + "/webpage/signup.html");
+});
+
+app.post("/login", urlencodedparser, (req, res)=>{
+    console.log(req.body.id);
+    console.log(req.body.password);
+    res.send(req.body);
 });
