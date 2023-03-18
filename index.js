206e57d1e2707aba39c813431e6af81bdce8f72d
Pranav <1pranavvyas@gmail.com>
2023-03-18 03:17:56 -0400

    Started the backend

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+var PORT = process.env.PORT || 8080;
+
+const server = http.Server(app).listen(PORT);
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/signup.html");
+});
